d2d3c3da040ab70ddce6586b3bfd44d7c24657b1
sushil <sushilshah3344@gmail.com>
2023-03-28 18:43:50 +0530

    nested Interface

---
+++ FirstProgram/src/com/tns/nestedinterface/MyClass.java
@@ -0,0 +1,18 @@
+package com.tns.nestedinterface;
+
+public class MyClass implements Outer{
+
+	@Override
+	public void m1() {
+		// TODO Auto-generated method stub
+		
+	}//implement only outer class
+//	public class MyClass implements Outer.inner{//implement only inner class
+//	@Override
+//	public void m2() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+
+}
---
+++ FirstProgram/src/com/tns/nestedinterface/Outer.java
@@ -0,0 +1,9 @@
+package com.tns.nestedinterface;
+
+public interface Outer {
+	void m1();
+	interface inner{
+		void m2();
+	}
+
+}
