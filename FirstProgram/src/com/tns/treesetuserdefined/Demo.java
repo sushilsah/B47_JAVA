d0a178cc34aabaa74f52148f15c726f08e7b90e8
sushil <sushilshah3344@gmail.com>
2023-04-14 16:59:36 +0530

    Tree UserDefined

--- FirstProgram/src/com/tns/treesetuserdefined/Demo.java
+++ FirstProgram/src/com/tns/treesetuserdefined/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TreeSet<Students> set = new TreeSet<Students>();
+		TreeSet<Students> set = new TreeSet<Students>(new Marks());
 		set.add(new Students(12,"rosy"));
 		set.add(new Students(13,"john"));
 		set.add(new Students(14,"mosh"));
